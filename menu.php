46168045b4b1001ae29a095962407da5e029767a
acansag <acansag@hotmail.com>
2013-02-24 23:23:28 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -9,30 +9,30 @@
                     <a class="brand" href="/">AFT</a>
                     <div class="nav-collapse collapse">
                         <ul class="nav">
-                            <li><a href="/duyuru">Duyurular</a></li>
+                            <li><a href="duyuru">Duyurular</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Kurslar <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
-                                    <li><a href="/kurs#tarih-icerik">Tarihler &amp; İçerik</a></li>
-                                    <li><a href="/kurs#katilim">Katılım</a></li>
+                                    <li><a href="kurs#tarih-icerik">Tarihler &amp; İçerik</a></li>
+                                    <li><a href="kurs#katilim">Katılım</a></li>
                                     <li class="divider"><li>
-                                    <li><a href="/kurs#kurallar">Kurallar</a></li>
-                                    <li><a href="/kurs#sss">SSS</a></li>
+                                    <li><a href="kurs#kurallar">Kurallar</a></li>
+                                    <li><a href="kurs#sss">SSS</a></li>
                                 </ul>
                             </li>
 
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">ODTÜ Fotoğraf Şenliği <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
-                                    <li><a href="/senlik#basvuru">Başvuru</a></li>
-                                    <li><a href="/senlik#barinma">Barınma</a></li>
-                                    <li><a href="/senlik#program">Program</a></li>
-                                    <li><a href="/senlik#katilim">Katılım</a></li>
+                                    <li><a href="senlik#basvuru">Başvuru</a></li>
+                                    <li><a href="senlik#barinma">Barınma</a></li>
+                                    <li><a href="senlik#program">Program</a></li>
+                                    <li><a href="senlik#katilim">Katılım</a></li>
                                 </ul>
                             </li>
-                            <li><a href="/portfolyo">Portfolyo</a></li>
-                            <li><a href="/iletisim">İletişim</a></li>
-<!--                            <li><a href="/arsiv">Arşiv</a></li>-->
+                            <li><a href="portfolyo">Portfolyo</a></li>
+                            <li><a href="iletisim">İletişim</a></li>
+                                   <!-- <li><a href="/arsiv">Arşiv</a></li>-->
                         </ul>
                     </div><!--/.nav-collapse -->
                 </div>
@@ -41,3 +41,4 @@
  
 
         <div class="container">
+        
